6799981b3ecb6a1f4d139c9eac29d69836d9a7c4
Ian Jørgensen <jorgensen.ian@gmail.com>
2011-07-17 23:34:46 +0200

    added about

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var server = require('router').create();
+var port = process.argv[2] || 9000;
 
 server.file('/s/{file}','./s/{file}');
 
 server.file('/','./s/index.html');
+server.file('/about','./s/about.html');
 server.file('/docs','./s/docs.html');
 server.file('/slides', './s/nodecamp.html');
 server.file('/pubsub.io.js','./s/pubsub.io.js');
 
 server.file('./s/404.html',{status:404});
 
-server.listen(process.argv[2] || 9000);
+server.listen(port);
+console.log('server running on port',port);
 
 process.on('uncaughtException', function(err) { console.log(err.stack) });
