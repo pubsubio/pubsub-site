bf31318e38bbc3dfe49f515e9f90a76bd4c29a72
Mathias Buus <mathiasbuus@gmail.com>
2011-07-24 16:53:34 +0200

    id gen bug

--- s/index.js
+++ s/index.js
@@ -1,2 +1,3 @@
-var id = document.getElementById('hello-world');
+var id = document.getElementsByTagName('pre')[0];
+
 id.innerHTML = id.innerHTML.replace(/\$sub/g, Math.floor(Math.random() * 9999999));
